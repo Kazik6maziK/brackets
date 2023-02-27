deaeb4f864441a1a08694e54d1fa218719994db6
Kazik6maziK <dimaredlin715@gmail.com>
2023-02-27 21:01:27 +0300

    fix: task brackets

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  let brackets = bracketsConfig;
-  let stack = []
-  let flag = true
-  for (let bracket of str) {
-    let bracketsIndex = brackets.indexOf(bracket)
-    if (bracketsIndex === -1){
-      continue
-    }
-
-    if(bracketsIndex % 2 === 0) {
-      stack.push(bracketsIndex + 1)
-    } else {
-      if(stack.pop() !== bracketsIndex) {
-        flag = false;
+  function Search() {
+    for (let i = 0; i < newArray.length;) 
+    {
+      if (str.indexOf(newArray[i]) === -1) 
+      {
+        i++;
+      } else if (str.indexOf(newArray[i]) !== -1) 
+      {
+        let itemNumber = str.indexOf(newArray[i]);
+        str = str.slice(0, itemNumber) + str.slice(itemNumber + 2);
+        i = 0;
       }
     }
-  }
-  return flag;
+}
+
+let newArray = [];
+for (let j = 0; j < bracketsConfig.length; j++) 
+{
+  let filled = bracketsConfig[j][0] + bracketsConfig[j][1];
+  newArray.push(filled);
+}
+
+
+Search();
+if (str.length > 0) 
+{
+  return false;
+} else 
+{
+  return true;
+}
 }
