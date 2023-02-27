c63b84db96f8291bc59c063f7f2b6652fa158928
Kazik6maziK <dimaredlin715@gmail.com>
2023-02-27 20:13:20 +0300

    init: task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = bracketsConfig;
+  let stack = []
+  let flag = true
+  for (let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket)
+    if (bracketsIndex === -1){
+      continue
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        flag = false;
+      }
+    }
+  }
+  return flag;
 }
